57a94ee8c733771e59f54893ce26b3018c210c9a
Geoffrey Parry <geoffreyp7@gmail.com>
2019-08-09 15:59:12 +1000

    Begin config

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteTitle: "GPG - full-stack dev and ML blog", // <title>
+  shortSiteTitle: "full-stack dev and ML blog", // <title> ending for posts and pages
+  siteDescription: "full-stack dev and ML blog",
   siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Geoffrey Parry-Grass",
+  authorTwitterAccount: "geoffreyp7",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "GeoffPG",
+  headerSubTitle: "full-stack dev and ML blog",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "GPG - full-stack dev and ML blog",
+  manifestShortName: "GPG Blog", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -28,7 +28,7 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://s.gravatar.com/avatar/639afd4612a14aee883a83569596d5f2?s=80",
 
   // social
   authorSocialLinks: [
